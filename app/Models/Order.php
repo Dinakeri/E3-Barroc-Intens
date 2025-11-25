e50d093435d83b798ef1792f458fd0764298f5e0
ericokwabi <deo0006@edu.curio.nl>
2025-11-25 12:09:30 +0100

    Made pivot table for customers and orders
    Made a pivot table orderitem to connect a customer with their orders. Added some test data to check if everything works.

---
+++ app/Models/Order.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Testing\Fluent\Concerns\Has;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'customer_id',
+        'order_date',
+        'total_amount',
+    ];
+
+    public function customer() {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function products() {
+        return $this->hasMany(Product::class);
+    }
+
+    public function orderItems() {
+        return $this->hasMany(OrderItem::class);
+    }
+}
--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Order;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,18 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->word(),
+            'type' => $this->faker->word(),
+            'cost_price' => $this->faker->randomFloat(2, 5, 500),
+            'sales_price' => $this->faker->randomFloat(2, 10, 1000),
+            'description' => $this->faker->sentence(),
+            'stock' => $this->faker->numberBetween(0, 100),
+            'status' => $this->faker->randomElement(['active', 'phased_out']),
+            'length' => $this->faker->numberBetween(1, 200),
+            'width' => $this->faker->numberBetween(1, 200),
+            'breadth' => $this->faker->numberBetween(1, 200),
+            'weight' => $this->faker->randomFloat(2, 0.1, 50),
+            'order_id' => Order::inRandomOrder()->first()->id,
         ];
     }
 }
