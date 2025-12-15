817b348c0d754ff7ac319913a17bedf5258eee25
ericokwabi <deo0006@edu.curio.nl>
2025-12-15 10:14:33 +0100

    Implemented sales sidebar partial on customers index, create and show pages page

--- resources/views/customers/create.blade.php
+++ resources/views/customers/create.blade.php
@@ -12,22 +12,7 @@
 
 
     @section('sidebar')
-        <flux:navlist class="w-64">
-            <flux:navlist.item href="{{ route('dashboards.sales') }}" class="mb-4" icon="home">Home</flux:navlist.item>
-            <flux:navlist.item href="#" class="mb-4" icon="building-storefront">Orders</flux:navlist.item>
-            <flux:navlist.item href="#" class="mb-4" icon="currency-dollar">Products</flux:navlist.item>
-            <flux:navlist.item href="{{ route('customers.index') }}" class="mb-4" icon="user">Customers
-            </flux:navlist.item>
-            <flux:navlist.item href="#" class="mb-4" icon="chart-bar">Reports</flux:navlist.item>
-            <flux:navlist.item href="" class="mb-4" icon="cog">Settings</flux:navlist.item>
-            <flux:navlist.item href="{{ route('customers.create') }}" class="mb-4" icon="plus">Add new customer
-            </flux:navlist.item>
-
-            <flux:spacer class="my-4 border-t border-neutral-700"></flux:spacer>
-            <flux:navlist.item href="#" class="mb-4 mt-auto" icon="arrow-left-end-on-rectangle">Logout
-            </flux:navlist.item>
-
-        </flux:navlist>
+        @include('partials.salesSidebar')
     @endsection
 
     {{-- Assuming you have installed Flux UI and included @fluxAppearance & @fluxScripts in your layout. :contentReference[oaicite:1]{index=1} --}}
--- resources/views/customers/show.blade.php
+++ resources/views/customers/show.blade.php
@@ -7,22 +7,7 @@
 
 
     @section('sidebar')
-        <flux:navlist class="w-64">
-            <flux:navlist.item href="{{ route('dashboards.sales') }}" class="mb-4" icon="home">Home</flux:navlist.item>
-            <flux:navlist.item href="#" class="mb-4" icon="building-storefront">Orders</flux:navlist.item>
-            <flux:navlist.item href="#" class="mb-4" icon="currency-dollar">Products</flux:navlist.item>
-            <flux:navlist.item href="{{ route('customers.index') }}" class="mb-4" icon="user">Customers
-            </flux:navlist.item>
-            <flux:navlist.item href="#" class="mb-4" icon="chart-bar">Reports</flux:navlist.item>
-            <flux:navlist.item href="" class="mb-4" icon="cog">Settings</flux:navlist.item>
-            <flux:navlist.item href="{{ route('customers.create') }}" class="mb-4" icon="plus">Add new customer
-            </flux:navlist.item>
-
-            <flux:spacer class="my-4 border-t border-neutral-700"></flux:spacer>
-            <flux:navlist.item href="{{ route('logout') }}" class="mb-4 mt-auto" icon="arrow-left-end-on-rectangle">Logout
-            </flux:navlist.item>
-
-        </flux:navlist>
+        @include('partials.salesSidebar')
     @endsection
 
     <div class="grid gap-4 lg:grid-cols-2 sm:grid-cols-1 mb-8">
