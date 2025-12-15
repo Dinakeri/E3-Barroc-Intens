e683e2e4278f55bf7ac78b5d295b837f85ac4755
ericokwabi <deo0006@edu.curio.nl>
2025-12-15 10:02:31 +0100

    Added payment url on finance sidebar
    Added payment link to finance sidebar to show on website and changed some icons of other urls

--- resources/views/partials/FinanceSidebar.blade.php
+++ resources/views/partials/FinanceSidebar.blade.php
@@ -1,9 +1,9 @@
 <flux:navlist class="w-64">
     <flux:navlist.item href="{{ route('dashboard') }}" class="mb-4" icon="home">Home</flux:navlist.item>
-    <flux:navlist.item href="{{ route('dashboards.contracts') }}" class="mb-4" icon="building-storefront">Contracten</flux:navlist.item>
-    <flux:navlist.item href="{{ route('invoices.index') }}" class="mb-4" icon="building-storefront">Facturen</flux:navlist.item>
+    <flux:navlist.item href="{{ route('dashboards.contracts') }}" class="mb-4" icon="document-duplicate">Contracten</flux:navlist.item>
+    <flux:navlist.item href="{{ route('invoices.index') }}" class="mb-4" icon="document-text">Facturen</flux:navlist.item>
     <flux:navlist.item href="{{ route('invoices.create') }}" class="mb-4" icon="plus">Maak factuur</flux:navlist.item>
-    <flux:navlist.item href="#" class="mb-4" icon="building-storefront">Betalingen</flux:navlist.item>
+    <flux:navlist.item href="{{ route('payments.index') }}" class="mb-4" icon="currency-euro">Betalingen</flux:navlist.item>
     <flux:spacer class="my-4 border-t border-neutral-700"></flux:spacer>
     <flux:navlist.item href="#" class="mb-4 mt-auto" icon="arrow-left-end-on-rectangle">Logout</flux:navlist.item>
 </flux:navlist>
