fab563512d43a0c7cea6b1b8bd3cba1b1dc7492a
ericokwabi <deo0006@edu.curio.nl>
2026-01-06 11:04:37 +0100

    Made changes to contract model
    Added extra fillable fields and defined database relational functions

--- app/Models/Contract.php
+++ app/Models/Contract.php
@@ -2,13 +2,36 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Testing\Fluent\Concerns\Has;
 
 class Contract extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
-        'customer',
-        'products',
-        'accesories',
+        'customer_id',
+        'quote_id',
+        'start_date',
+        'end_date',
+        'total_amount',
+        'status',
+        'pdf_path',
     ];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function quote()
+    {
+        return $this->belongsTo(Quote::class);
+    }
+
+    public function invoices()
+    {
+        return $this->hasMany(Invoice::class);
+    }
 }
