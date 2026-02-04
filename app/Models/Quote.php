f338186a35bc87351dbed92c17e546c89a709c34
ericokwabi <deo0006@edu.curio.nl>
2026-02-04 13:32:40 +0100

    Added new relationship to quote model
    Added new order relationship to model

--- app/Models/Quote.php
+++ app/Models/Quote.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Quote extends Model
 {
     /** @use HasFactory<\Database\Factories\QuoteFactory> */
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'customer_id',
@@ -25,6 +26,10 @@ class Quote extends Model
     public function contract()
     {
         return $this->hasOne(Contract::class);
+    }
+
+    public function order(){
+        return $this->belongsTo(Order::class);
     }
 
     public function items()
