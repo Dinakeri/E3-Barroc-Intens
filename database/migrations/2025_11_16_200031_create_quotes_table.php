1ccb4a8a7cea827697f30c9bd8a3777882101c56
ericokwabi <deo0006@edu.curio.nl>
2026-02-10 11:01:26 +0100

    Added order_id to quotes database migration and implemented changes to quote factory

--- database/migrations/2025_11_16_200031_create_quotes_table.php
+++ database/migrations/2025_11_16_200031_create_quotes_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('quotes', function (Blueprint $table) {
             $table->id();
             $table->foreignId('customer_id')->constrained('customers')->onDelete('cascade');
+            $table->foreignId('order_id')->constrained('orders')->onDelete('cascade');
             $table->date('valid_until')->nullable();
             $table->integer('total_amount')->default(0);
             $table->enum('status', ['draft', 'sent', 'approved', 'rejected'])->default('draft');
